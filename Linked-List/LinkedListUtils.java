3e3d80c8849e8cf1ddc60d7ea9448d7305ed5e28
Robinson Abel Douglas <166375742+Douglas0207@users.noreply.github.com>
2025-06-26 23:32:52 +0530

    Add files via upload

--- Linked-List/LinkedListUtils.java
+++ Linked-List/LinkedListUtils.java
@@ -2,22 +2,23 @@ public class LinkedListUtils
 {
     public static Node createNode(int arr[])
     {
-        Node head = new Node(arr[0],null);
-        Node mover = head;
-        for(int i=1;i<arr.length;i++)
-        {
-            Node temp = new Node(arr[i],null);
-            mover.next = temp;
-            mover = temp;
-        }
-        return head;
+      Node head = new Node(arr[0], null, null);
+      Node mover = head;
+      for(int i=1;i<arr.length;i++)
+      {
+        Node temp = new Node(arr[i], null, mover);
+        mover.next = temp;
+        mover = temp;
+      }
+      return head;
     }
-    public static void printList(Node head)
+    public static void printNode(Node head)
     {
         Node temp = head;
+        System.out.print("NULL<-->");
         while(temp!=null)
         {
-            System.out.print(temp.data+"->");
+            System.out.print(temp.data+"<-->");
             temp = temp.next;
         }
         System.out.println("NULL");
